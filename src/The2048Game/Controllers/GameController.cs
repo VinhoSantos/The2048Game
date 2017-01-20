4759a8f02c1f2e169a6ba3e713020ce07dad195f
Bart Vets <bart_vets@hotmail.com>
2017-01-20 22:09:51 +0100

    initial layout of square with sample data

---
+++ src/The2048Game/Controllers/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using The2048Game.Models.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace The2048Game.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var model = new GameViewModel()
+            {
+                GameState = new int?[4, 4] { { 4096, 2048, 1024, 512 }, { 256, 128, 64, 32 }, { 16, 8, 4, 2 }, { 2, 2, null, null } }
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ src/The2048Game/Models/ViewModels/GameViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace The2048Game.Models.ViewModels
+{
+    public class GameViewModel
+    {
+        public int?[,] GameState { get; set; }
+
+    }
+}
